59f9d7177be2e829468610233726434daafd7f26
SergWebAcceptance <serg.web@acceptance.ltd>
2025-06-11 15:38:40 +0300

    added localization

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'de', 'it'],
+  locales: ['en', 'de', 'it', 'et'],
   localePrefix: 'as-needed',
   // Used when no locale matches
   defaultLocale: 'en',
